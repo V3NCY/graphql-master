5cee0b35a5e82cae8eb19dd092830807fe99c201
V3NCY <venceslavam@gmail.com>
2021-05-22 00:02:45 +0300

    editted hardcodedrestaurants

--- graphql/resolvers/hardcodedrestaurants.js
+++ graphql/resolvers/hardcodedrestaurants.js
@@ -1,244 +1,99 @@
 export default {
-  "hotels": [
+  "restaurants": [
     {
       "id": 1,
-      "name": "Restaurant",
+      "name": "Ресторант Дядо Либен",
       "stars": 4,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/764/76482925.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/616/61660857.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/699/69935787.jpg"
+        "https://lh3.googleusercontent.com/proxy/KWN4rEnWlKRhGC4TiFR2in5F6583ovO_b67F9aNlp_wtvmwPmTaA6Z3xQa_JeYlCFvCJMJ8ze9R1BexGtpCPthPHh7Ionb5wftpQRylUpeYkKFLMjawY3Fghp75FyQ",
+        "https://businessportal.bg/wp-content/uploads/2016/04/img_8179_12733620-2.jpg",
+        "https://comunicatorbg.com/dqdo_luben/mehana.jpg"
       ],
-      "description": "Стоичковата къща се намира в Копривщица и предлага съоръжения за барбекю и гледка към планината. Разполага също с детска площадка и СПА зона, а гостите могат да се насладят на ястията в ресторанта и напитките в бара.",
+      "description": "Ресторант Дядо Либен разполага с богато меню с традиционни ястия.",
       "city": "Копривщица"
     },
     {
       "id": 2,
-      "name": "Маврудиева къща",
-      "stars": 2,
+      "name": "Ресторант България",
+      "stars": 5,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/118/118996743.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/119/119027934.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/119/119021889.jpg"
+        "https://imgrabo.com/pics/guide/900x600/20201215164801_58582.jpeg",
+        "http://restaurantbulgaria.weebly.com/uploads/4/2/2/7/42270081/3741874.jpg?602",
+        "http://restaurantbulgaria.weebly.com/uploads/4/2/2/7/42270081/5018427_orig.jpg"
       ],
-      "description": "Къща за гости Маврудиева в Копривщица е разположена до гора, само на 2 минути пеша от центъра на града. Гостите могат да посетят различни ресторанти, музеи, магазини, кафенета и барове.",
+      "description": "Българска и европейска кухня.",
       "city": "Копривщица"
     },
     {
       "id": 3,
-      "name": "Емили Фемили Хаус",
+      "name": "Пицария Копривцщица",
       "stars": 3,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/274/274432630.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/274/274432635.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/274/274432640.jpg"
+        "http://restaurantbulgaria.weebly.com/uploads/4/2/2/7/42270081/5018427_orig.jpg",
+        "https://lh3.googleusercontent.com/proxy/WtrtoMiMf6_WMYIS4iXtEmrbzTOAS4gBRXltcltesWANkDTjnTcDT3s5hsl5yOQk-7SN_WCAKtaFey4mqxCkqPRD6MHXK2hV9g",
+        "https://d3u845fx6txnqz.cloudfront.net/news/i327-Where-is-the-most-delicious-pizza-in-Gabrovo-.jpg"
       ],
-      "description": "Къща за гости Емили Фемили Хаус се намира в Копривщица, на по-малко от 1 км от къщата-музей на Тодор Каблешков и предлага градина, тераса и изглед към града. Къщата за гости разполага с фамилни стаи.",
+      "description": "Пици по ВАШ вкус.",
       "city": "Копривщица"
     },
     {
       "id": 4,
-      "name": "Къща за гости Денис",
+      "name": "Механа Старата Круша",
       "stars": 2,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/482/48211601.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/399/39976840.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/132/132635905.jpg"
+        "http://b2bridges.eu/images/restaurants/4//Pod%20starata%20krusha.jpg",
+        "https://imgrabo.com/pics/guide/900x600/20201215172958_98290.jpeg",
+        "https://lh3.googleusercontent.com/proxy/OicXXo4cquz0nBYH_rJzbzgswsZbDQKTUNwxZoyYfVYth5frC0_ggQms2Q9wPPxYkbJC7YU40xIRFet_L0x23E8jsc6gOimXhrs"
       ],
-      "description": "Къща за гости Денис се намира в Копривщица и предлага безплатен WiFi и градина с барбекю. На място има безплатен паркинг.",
+      "description": "Традиционно приготвено пиво с богато меню.",
       "city": "Копривщица"
     },
     {
       "id": 5,
-      "name": "Къща за гости Лина",
+      "name": "Хотел-ресторант Чучура",
       "stars": 3,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/351/35146416.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/755/75514508.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/359/35990460.jpg"
+        "https://cf.bstatic.com/images/hotel/max1280x900/757/75733105.jpg",
+        "https://lh3.googleusercontent.com/proxy/oJ8vV5nvwTu1saA9nb8qDVZzHApALvzvw10Xknio6lG2fPRuk4eMGrOrEI6_c-8zFOsHKIvNm84LJY3HZunaaaihsqqKKvkhLU_QFPEl7BVcPe95ltFpfNmkAISW7iOnuoSgxu-owl9e04E",
+        "https://cf.bstatic.com/images/hotel/max1024x768/143/143838446.jpg"
       ],
-      "description": "Къща за гости Лина се намира на 300 м от центъра на Копривщица и предлага безплатен WiFi интернет.",
+      "description": "На най-удобното място - центъра с богато меню и незабравими вкусове.",
       "city": "Копривщица"
     },
     {
       "id": 6,
-      "name": "Юруковата къща",
+      "name": "Механа Ломева къща",
       "stars": 3,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/164/164451235.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/164/164453211.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/164/164452917.jpg"
+        "https://gozbarovhouse.com/wp-content/uploads/2010/03/koprivshtitsa-restaurant-gozbarov-07.jpg",
+        "https://gozbarovhouse.com/wp-content/uploads/2010/03/koprivshtitsa-restaurant-gozbarov-08.jpg",
+        "https://gozbarovhouse.com/wp-content/uploads/2010/03/koprivshtitsa-restaurant-gozbarov-04.jpg"
       ],
-      "description": "Къща за гости Юруковата къща разполага с бар, градина и гледка към реката. Разположена е в Копривщица, на 300 метра от къщата - музей на Тодор Каблешков. Всички стаи са оборудвани с телевизор с плосък екран с кабелни канали и собствена баня. В къщата за гости има фамилни стаи.",
+      "description": "Механата е с капацитет от 50 маси.",
       "city": "Копривщица"
     },
     {
       "id": 7,
-      "name": "Къща за Гости Роден Дом",
+      "name": "Механа При Бай Генчо",
       "stars": 2,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/268/268252142.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/268/268250197.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/268/268376651.jpg"
+        "https://comunicatorbg.com/baigencho/baigencho-1472227586-05.jpg",
+        "https://scontent.fymy1-2.fna.fbcdn.net/v/t31.18172-0/p180x540/13731941_1248742258477209_141051277514941196_o.jpg?_nc_cat=111&ccb=1-3&_nc_sid=2d5d41&_nc_ohc=_LzLaYGyrysAX_mDuvi&_nc_ht=scontent.fymy1-2.fna&tp=6&oh=d1c46858fe833853771eb627282ba531&oe=60AC2F71",
+        "https://lh3.googleusercontent.com/p/AF1QipMzvxiM5ETU_Tm3EqblGTMwqF9wWl_MxJ1MveQH=w600-k"
       ],
-      "description": "Къща за гости Роден дом се намира на спокойно място на 200 метра от центъра на Копривщица. Тя предлага стаи с безплатен Wi-Fi и обща тераса с печка на дърва, от която има прекрасен изглед към околната гора и планините.",
+      "description": "На изхода на Копривщица.",
       "city": "Копривщица"
     },
     {
       "id": 8,
-      "name": "Red House",
+      "name": "Ресторант Галерия",
       "stars": 4,
       "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/136/136847395.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/131/131599271.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/276/276067006.jpg"
+        "https://www.galeria-koprivshtica.com/wp-content/uploads/16923659_943818069087984_321741987_n.jpg",
+        "https://www.ski359.com/common_images/offers/4357/bc1f5bc880c596032e5f4a8e72580ce2.jpg",
+        "https://grupovo.net/pictures/main/2019/05/16/m_noshtuvka-na-chovek-sas-zakuska-obyad-i-vecherya-nov-baseyn-i-relaks-zona-v-kompleksgaleriya.jpg"
       ],
-      "description": "Ваканционни къщи Red House предлага изглед към градината, безплатен WiFi и безплатен собствен паркинг. Намира се в Копривщица, на 700 метра от къщата - музей на Тодор Каблешков.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 9,
-      "name": "Къща за гости Боби",
-      "stars": 2,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/353/35312094.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/902/90269623.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/832/83228341.jpg"
-      ],
-      "description": "Къща за гости Боби се намира на 100 метра от историческия център на град Копривщица. Разполага с подканяща градина, стаи със самостоятелна баня, кабелна телевизия, тераса и обща кухня.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 10,
-      "name": "Парк-Хотел Орлов Камък",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/940/94048948.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/851/85151451.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/230/230833622.jpg"
-      ],
-      "description": "Парк-Хотел Орлов Камък се намира в Копривщица, на 6 км от Къщата-музей на Тодор Каблешков и предлага помещения за настаняване с ресторант, безплатен частен паркинг, бар и общ салон. На разположение на гостите има градина и тераса. Мястото за настаняване предлага детска площадка, денонощна рецепция и безплатен WiFi във всички части.",
-      "city": "Копривщица"
-    },
-
-    {
-      "id": 11,
-      "name": "Чолаковата Къща",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/154/154415437.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/171/171774341.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/139/139917436.jpg"
-      ],
-      "description": "Къща за гости ЧОЛАКОВАТА КЪЩА се намира в Копривщица, на по-малко от 1 км от къщата-музей на Тодор Каблешков. Предлага помещения за настаняване с общ салон, безплатен собствен паркинг, градина и съоръжения за барбекю. Предлага фамилни стаи и тераса. Има обща кухня и безплатен WiFi навсякъде.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 12,
-      "name": "Family Vacation Home Topolnitza",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/102/102967029.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/102/102967071.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/102/102967052.jpg"
-      ],
-      "description": "Vemi Family Vacation Home е с изглед към планината и се намира на 3 минути пеша от къщата-музей „Тодор Каблешков“ и на 5 минути пеша от центъра на града. На разположение на гостите има тераса.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 13,
-      "name": "Хаджи Нейкови къщи за гости",
-      "stars": 2,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/546/5468277.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/214/214665206.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/198/198790645.jpg"
-      ],
-      "description": "Двете къщи за гости Хаджи Нейкови са построени в стил, характерен за периода на Българското национално възраждане. Къщите се намират на терена на добре поддържан двор, на 500 метра от историческия център на Копривщица. Осигурен е безплатен Wi-Fi интернет.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 14,
-      "name": "Къща за Гости Дона - Конна База",
-      "stars": 2,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/261/26121995.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/135/135709252.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/222/222255305.jpg"
-      ],
-      "description": "Къща за гости Дона се намира в Копривщица и предлага безплатен WiFi и безплатен собствен паркинг на място.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 15,
-      "name": "Комплекс Дюлгерите",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/441/4416464.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/178/178906865.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/173/173152748.jpg"
-      ],
-      "description": "Комплекс Дюлгерите се намира в историческия център на Копривщица. Състои се от вили тип котидж със стаи със собствени балкони и обща градина.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 16,
-      "name": "Гозбарова Kъща за Гости",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/563/5630281.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/555/5552524.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/563/5630300.jpg"
-      ],
-      "description": "Гозбарова къща за гости в Копривщица предлага стаи, боядисани в топли цветове, които са с мебели и настилки от тъмно дърво. Стаите имат тавани с дървена ламперия. Всички картини, тапицерии, килими и много други декоративни елементи са ръчно изработени.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 17,
-      "name": "Семеен Хотел Калина",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/256/25676419.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/256/25676576.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/256/25676988.jpg"
-      ],
-      "description": "Семеен хотел Калина е с централно местоположение във възрожденския град Копривщица и предлага стаи с безплатен Wi-Fi интернет. Разполага с градина с открит кът за сядане.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 18,
-      "name": "Семеен Хотел Чучура",
-      "stars": 2,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/757/75733105.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/140/14094738.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/143/143838446.jpg"
-      ],
-      "description": "Семеен хотел Чучура предлага безплатен Wi-Fi интернет и прекрасна тераса с градина. Този традиционен хотел се намира на 50 метра от централния площад на град Копривщица.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 19,
-      "name": "Семеен Хотел Бащина Къща",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/110/110576259.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/217/217645594.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/196/196080767.jpg"
-      ],
-      "description": "Family Hotel Bashtina Kashta се намира на 50 метра от площад 20-ти Април в Копривщица и разполага с голям ресторант с камина и градинска тераса. Сервира традиционни ястия от региона и предлага закуска всяка сутрин.",
-      "city": "Копривщица"
-    },
-    {
-      "id": 20,
-      "name": "Радкината къща",
-      "stars": 3,
-      "images": [
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/267/267302577.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/199/199654228.jpg",
-        "https://t-cf.bstatic.com/images/hotel/max1280x900/203/203725420.jpg"
-      ],
-      "description": "Разположена в градина, тераса и гледка към градината, Радкината къща есе намира в Копривщица, на по-малко от 1 км от мемориалната къща на Тодор Каблешков. Осигурени са обща кухня и безплатен WiFi навсякъде.",
+      "description": "Хотел-ресторант Галерия разполага както с обширен затворен ресторант, така и с лятна градина, а менюто е не по-малко богато.",
       "city": "Копривщица"
     }
   ]
